7643d071451abd14a3dd38eaa37d8264289a4bbd
AnthonyCallo04 <166783354+anthonycallo04@users.noreply.github.com>
2024-09-30 00:19:31 -0500

    Artwork Api

---
+++ script.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function($) {
+        setInterval(function() {
+            $.get('https://live.radiorockhits.online/api/nowplaying/radiorockhits')
+                .done(function(data) {
+
+                    const coverImageUrl = data.now_playing.song.art || 'assets/Logoradiorockhits.webp';
+                    $('#cover-image').attr('src', coverImageUrl);
+
+                    $('#song-title').text(data.now_playing.song.text || 'Cargando...');
+
+                })
+                .fail(function() {
+                    $('#cover-image').attr('src', 'assets/Logoradiorockhits.webp');
+
+                    $('#song-title').text('Cargando...');
+                })
+        }, 1000);
+    });
